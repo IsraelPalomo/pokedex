4c364f95fb2c62a4a50746cba44ce4bbd9599791
IsraelPalomo <78044199+IsraelPalomo@users.noreply.github.com>
2021-08-15 11:33:46 +0200

    Limpieza completa de proyecto

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,14 @@
 <template>
-	<div class="home">
-		<div class="titulo">
-			<img class="titulo--pokedex" src="../assets/titulo_principal.png" alt="" />
-		</div>
-		<Navegacion />
-	</div>
+	<div class="home"></div>
 </template>
 
 <script>
-import Navegacion from "../components/Navegacion.vue";
 // @ is an alias to /src
 
 export default {
 	name: "Home",
-	components: { Navegacion },
+	components: {},
 };
 </script>
 
-<style lang="scss" scoped>
-.home {
-	.titulo {
-		display: flex;
-		justify-content: center;
-		margin-top: 5rem;
-		.titulo--pokedex {
-			width: 80%;
-			margin: 0 auto;
-		}
-	}
-}
-</style>
+<style lang="scss" scoped></style>
