09cb4e882eeb41336a645a4cfd3ecbb47a6f1f36
IsraelPalomo <78044199+IsraelPalomo@users.noreply.github.com>
2021-08-21 15:15:56 +0200

    Añadidos estilos paginas pokemon

--- src/components/PokemonComponenteVista.vue
+++ src/components/PokemonComponenteVista.vue
@@ -4,17 +4,16 @@
 		<div class="contenedor-1">
 			<div class="imagenes">
 				<img class="imagenes__1" :src="pokemon.sprites.front_default" alt="" />
-				<img class="imagenes__2" :src="pokemon.sprites.front_shiny" alt="" />
 			</div>
 			<div class="medidas">
 				<span> Altura : {{ pokemon.height }} m </span>
-				<br />
+
 				<span> Peso : {{ pokemon.weight }} kg </span>
-				<br />
+
 				<span>
 					Sexo: Masculino Femenino
 				</span>
-				<br />
+
 				<span>
 					Habilidad :
 					{{
@@ -23,110 +22,111 @@
 					}}
 				</span>
 			</div>
-			<div class="tipos">
-				<p
-					class="tipo1"
-					:style="
-						pokemon.types[0].type.name == 'fire'
-							? 'background: #C72100'
-							: pokemon.types[0].type.name == 'grass'
-							? 'background:#66BA2A'
-							: pokemon.types[0].type.name == 'electric'
-							? 'background:#FBBA17'
-							: pokemon.types[0].type.name == 'water'
-							? 'background:#106CC7'
-							: pokemon.types[0].type.name == 'ground'
-							? 'background:#CFAE52'
-							: pokemon.types[0].type.name == 'rock'
-							? 'background:#9D863C'
-							: pokemon.types[0].type.name == 'fairy'
-							? 'background:#F0A9F0'
-							: pokemon.types[0].type.name == 'poison'
-							? 'background:#7D327E'
-							: pokemon.types[0].type.name == 'bug'
-							? 'background:#88960D'
-							: pokemon.types[0].type.name == 'dragon'
-							? 'background:#9481EB'
-							: pokemon.types[0].type.name == 'psychic'
-							? 'background:#D33367'
-							: pokemon.types[0].type.name == 'flying'
-							? 'background:#90A2F0'
-							: pokemon.types[0].type.name == 'fighting'
-							? 'background:#79301A'
-							: pokemon.types[0].type.name == 'normal'
-							? 'background:#C3BDB3'
-							: pokemon.types[0].type.name == 'dark'
-							? 'background:#0E0E0E'
-							: pokemon.types[0].type.name == 'ghost'
-							? 'background:#5E5EB0'
-							: pokemon.types[0].type.name == 'ice'
-							? 'background:#82DBF8'
-							: pokemon.types[0].type.name == 'steel'
-							? 'background:#9797A7'
-							: ''
-					"
-				>
-					{{
-						pokemon.types[0].type.name.charAt(0).toUpperCase() + pokemon.types[0].type.name.slice(1)
-					}}
-				</p>
-				<p
-					class="tipo2"
-					v-if="pokemon.types.length == 2"
-					:style="
-						pokemon.types[1].type.name == 'fire'
-							? 'background: #C72100'
-							: pokemon.types[1].type.name == 'grass'
-							? 'background:#66BA2A'
-							: pokemon.types[1].type.name == 'electric'
-							? 'background:#FBBA17'
-							: pokemon.types[1].type.name == 'water'
-							? 'background:#106CC7'
-							: pokemon.types[0].type.name == 'ground'
-							? 'background:#CFAE52'
-							: pokemon.types[1].type.name == 'rock'
-							? 'background:#9D863C'
-							: pokemon.types[1].type.name == 'fairy'
-							? 'background:#F0A9F0'
-							: pokemon.types[1].type.name == 'poison'
-							? 'background:#7D327E'
-							: pokemon.types[1].type.name == 'bug'
-							? 'background:#88960D'
-							: pokemon.types[1].type.name == 'dragon'
-							? 'background:#9481EB'
-							: pokemon.types[1].type.name == 'psychic'
-							? 'background:#D33367'
-							: pokemon.types[1].type.name == 'flying'
-							? 'background:#90A2F0'
-							: pokemon.types[1].type.name == 'fighting'
-							? 'background:#79301A'
-							: pokemon.types[1].type.name == 'normal'
-							? 'background:#C3BDB3'
-							: pokemon.types[1].type.name == 'dark'
-							? 'background:#0E0E0E'
-							: pokemon.types[1].type.name == 'ghost'
-							? 'background:#5E5EB0'
-							: pokemon.types[1].type.name == 'ice'
-							? 'background:#82DBF8'
-							: pokemon.types[1].type.name == 'steel'
-							? 'background:#9797A7'
-							: ''
-					"
-				>
-					{{
-						pokemon.types[1].type.name.charAt(0).toUpperCase() + pokemon.types[1].type.name.slice(1)
-					}}
-				</p>
-			</div>
+		</div>
+		<div class="tipos">
+			<p
+				class="tipo1"
+				:style="
+					pokemon.types[0].type.name == 'fire'
+						? 'background: #C72100'
+						: pokemon.types[0].type.name == 'grass'
+						? 'background:#66BA2A'
+						: pokemon.types[0].type.name == 'electric'
+						? 'background:#FBBA17'
+						: pokemon.types[0].type.name == 'water'
+						? 'background:#106CC7'
+						: pokemon.types[0].type.name == 'ground'
+						? 'background:#CFAE52'
+						: pokemon.types[0].type.name == 'rock'
+						? 'background:#9D863C'
+						: pokemon.types[0].type.name == 'fairy'
+						? 'background:#F0A9F0'
+						: pokemon.types[0].type.name == 'poison'
+						? 'background:#7D327E'
+						: pokemon.types[0].type.name == 'bug'
+						? 'background:#88960D'
+						: pokemon.types[0].type.name == 'dragon'
+						? 'background:#9481EB'
+						: pokemon.types[0].type.name == 'psychic'
+						? 'background:#D33367'
+						: pokemon.types[0].type.name == 'flying'
+						? 'background:#90A2F0'
+						: pokemon.types[0].type.name == 'fighting'
+						? 'background:#79301A'
+						: pokemon.types[0].type.name == 'normal'
+						? 'background:#C3BDB3'
+						: pokemon.types[0].type.name == 'dark'
+						? 'background:#0E0E0E'
+						: pokemon.types[0].type.name == 'ghost'
+						? 'background:#5E5EB0'
+						: pokemon.types[0].type.name == 'ice'
+						? 'background:#82DBF8'
+						: pokemon.types[0].type.name == 'steel'
+						? 'background:#9797A7'
+						: ''
+				"
+			>
+				{{
+					pokemon.types[0].type.name.charAt(0).toUpperCase() + pokemon.types[0].type.name.slice(1)
+				}}
+			</p>
+			<p
+				class="tipo2"
+				v-if="pokemon.types.length == 2"
+				:style="
+					pokemon.types[1].type.name == 'fire'
+						? 'background: #C72100'
+						: pokemon.types[1].type.name == 'grass'
+						? 'background:#66BA2A'
+						: pokemon.types[1].type.name == 'electric'
+						? 'background:#FBBA17'
+						: pokemon.types[1].type.name == 'water'
+						? 'background:#106CC7'
+						: pokemon.types[0].type.name == 'ground'
+						? 'background:#CFAE52'
+						: pokemon.types[1].type.name == 'rock'
+						? 'background:#9D863C'
+						: pokemon.types[1].type.name == 'fairy'
+						? 'background:#F0A9F0'
+						: pokemon.types[1].type.name == 'poison'
+						? 'background:#7D327E'
+						: pokemon.types[1].type.name == 'bug'
+						? 'background:#88960D'
+						: pokemon.types[1].type.name == 'dragon'
+						? 'background:#9481EB'
+						: pokemon.types[1].type.name == 'psychic'
+						? 'background:#D33367'
+						: pokemon.types[1].type.name == 'flying'
+						? 'background:#90A2F0'
+						: pokemon.types[1].type.name == 'fighting'
+						? 'background:#79301A'
+						: pokemon.types[1].type.name == 'normal'
+						? 'background:#C3BDB3'
+						: pokemon.types[1].type.name == 'dark'
+						? 'background:#0E0E0E'
+						: pokemon.types[1].type.name == 'ghost'
+						? 'background:#5E5EB0'
+						: pokemon.types[1].type.name == 'ice'
+						? 'background:#82DBF8'
+						: pokemon.types[1].type.name == 'steel'
+						? 'background:#9797A7'
+						: ''
+				"
+			>
+				{{
+					pokemon.types[1].type.name.charAt(0).toUpperCase() + pokemon.types[1].type.name.slice(1)
+				}}
+			</p>
 		</div>
 		<div class="puntos_base">
-			HP : {{ pokemon.stats[0].base_stat }} <br />
-			Ataque : {{ pokemon.stats[1].base_stat }} <br />
-			Defensa : {{ pokemon.stats[2].base_stat }} <br />
-			Ataque-Especial : {{ pokemon.stats[3].base_stat }} <br />
-			Defensa-Especial : {{ pokemon.stats[4].base_stat }} <br />
-			Rapidez : {{ pokemon.stats[5].base_stat }} <br />
+			<span> HP : {{ pokemon.stats[0].base_stat }} </span>
+			<span> Ataque : {{ pokemon.stats[1].base_stat }} </span>
+			<span>Defensa : {{ pokemon.stats[2].base_stat }} </span>
+			<span> Ataque-Especial : {{ pokemon.stats[3].base_stat }} </span>
+			<span>Defensa-Especial : {{ pokemon.stats[4].base_stat }} </span>
+			<span>Rapidez : {{ pokemon.stats[5].base_stat }} </span>
 		</div>
+		<router-link to="/" class="router"><button>Volver a la Pokedex</button></router-link>
 	</div>
 </template>
 
@@ -166,16 +166,106 @@ export default {
 		padding: 1rem;
 		color: white;
 		background-color: #323232;
+		margin-bottom: 3rem;
 	}
-	.imagenes {
-		.imagenes__1 {
-			width: 25rem;
-			height: 25rem;
-			margin: 0 auto;
+	.contenedor-1 {
+		width: 80%;
+		background-color: #323232;
+		margin: 0 auto;
+		max-width: 40rem;
+		.imagenes {
+			.imagenes__1 {
+				margin: 0 auto;
+				display: block;
+				width: 15rem;
+				height: 15rem;
+				z-index: 99;
+			}
 		}
-		.imagenes__2 {
-			position: relative;
+		.medidas {
+			padding: 2rem;
+			background-color: rgb(43, 156, 201);
+
+			display: grid;
+			gap: 1rem;
+			grid-template-columns: 1fr;
+			color: white;
+			font-size: 1.3rem;
 		}
 	}
+	.tipos {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 2rem;
+		margin-top: 3rem;
+		.tipo1 {
+			padding: 0.5rem;
+			border-radius: 15px;
+			width: 5rem;
+			text-align: center;
+			color: #1d1b1b;
+			font-weight: 600;
+			font-size: 1.1rem;
+		}
+		.tipo2 {
+			padding: 0.5rem;
+			border-radius: 15px;
+			width: 5rem;
+			text-align: center;
+			color: #1d1b1b;
+			font-weight: 600;
+			font-size: 1.1rem;
+		}
+	}
+	.puntos_base {
+		padding: 2rem;
+		background-color: #323232;
+		text-align: center;
+		display: grid;
+		grid-template-columns: 1fr;
+		color: white;
+		gap: 1rem;
+		font-size: 1.3rem;
+		margin-top: 3rem;
+	}
+	button {
+		background-color: #da471b;
+		color: white;
+		padding: 0.5rem 1rem;
+		border: none;
+		border-radius: 7px;
+		margin: 5rem auto;
+
+		display: block;
+		cursor: pointer;
+
+		&:hover {
+			background-color: #9e3819;
+		}
+	}
+	.router {
+		text-decoration: none;
+	}
+}
+
+//TODO: MEDIA QUERYS
+
+@media (min-width: 450px) {
+}
+@media (min-width: 650px) {
+	.contenedor-1 {
+		display: flex;
+		.medidas {
+			width: 100%;
+		}
+	}
+}
+
+@media (min-width: 900px) {
+}
+@media (min-width: 1200px) {
+}
+@media (min-width: 2000px) {
 }
 </style>
