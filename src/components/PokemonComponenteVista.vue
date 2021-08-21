f29810031373116fe0bd1abaf5583301b2d1d3b9
IsraelPalomo <78044199+IsraelPalomo@users.noreply.github.com>
2021-08-21 12:50:48 +0200

    Añadida paginas unicas a cada pokemon gracias a las rutas dinamicas

--- src/components/PokemonComponenteVista.vue
+++ src/components/PokemonComponenteVista.vue
@@ -1,10 +1,36 @@
 <template>
-	<h1>componente {{ $route.params.id }}</h1>
+	<div>
+		<h1>componente {{ $route.params.id }}</h1>
+		<img :src="pokemon.sprites.front_default" alt="" />
+	</div>
 </template>
 
 <script>
 export default {
 	name: "PokemonComponenteVista",
+	data() {
+		return {
+			numeroPokemon: this.$route.params.id,
+			pokemon: {},
+		};
+	},
+	methods: {
+		async buscarPokemonUnico() {
+			try {
+				const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.numeroPokemon}`);
+				const datas = await res.json();
+				const datass = datas;
+				const data = datass;
+				this.pokemon = data;
+				console.log(data);
+			} catch (error) {
+				console.log(error);
+			}
+		},
+	},
+	created() {
+		this.buscarPokemonUnico();
+	},
 };
 </script>
 
